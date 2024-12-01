ace69173d181678d677505919da209d1c50115f0
Adominguez <dmartin.alberto@gmail.com>
2024-12-02 00:11:15 +0100

    chore: Remove unused imports and console.log statement in create-story API

--- src/pages/api/create-story.ts
+++ src/pages/api/create-story.ts
@@ -1,3 +1,12 @@
+// import { openai } from "@ai-sdk/openai";
+// import { generateObject } from "ai";
+// import { fullSchema } from "@src/schemas";
+// import { generateStorySetup, truncateString } from "@src/utils/characters";
+// import OpenAI from "openai";
+// import { v2 as cloudinary } from 'cloudinary'
+
+console.log();
+
 export function GET(request: Request) {
-  return new Response(`Hello from server function`);
+  return new Response(import.meta.env.TURSO_DATABASE_URL);
 }
