a6bde069f316a545d50af049cd05062796bb3c37
Adominguez <dmartin.alberto@gmail.com>
2024-12-11 22:18:05 +0100

    chore: Update npm dependencies and add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap(), tailwind(), react()],
     output: 'server',
     adapter: vercel({
         edgeMiddleware: true,
