02be06bfac5ddaec3e38dcd872f92d5d1abd6d6e
Adominguez <dmartin.alberto@gmail.com>
2024-12-16 15:32:08 +0100

    refactor: index page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': 'hsl(11.28deg 74.52% 69.22%)'
+			},
+		},
 	},
 	plugins: [],
 }
